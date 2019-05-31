dc2a652d6bb9f5ed5f4b5e97381f84e7ad952540
cometfish <me+git@cporter.dev>
2019-05-31 15:35:20 +1000

    Add driver to monitor online/offline status of the hub via MQTT

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Add MiLight/LimitlessLED bulbs to Hubitat (requires the [ESP8266 MiLight Hub](ht
     2. your hex Hub address (see the hub documentation for how to discover/set this)
     3. your light type (`cct` for a White-only bulb, `rgb` for an RGB bulb, or `rgb_cct` for an RGBW bulb)
     4. your light Group ID (or `0` for the 'All' group / `0` for an RGB bulb)
+
+You can also monitor the status of the MiLight hub if your hub is version 1.9.0 or higher and you have an MQTT broker (online/offline):
+1. Configure your ESP8266 MiLight Hub for MQTT:
+    1. Enter your MQTT broker address under `MQTT server`
+    2. Enter your desired topic under `MQTT Client Status Topic`, eg. `milight/status`
+2. Add `hubdriver.groovy` to your Hubitat as a new Driver (under `Drivers Code`)
+3. Add a new device to your Hubitat, set device Type to your User driver of 'MiLight-LimitlessLED Gateway'
+4. Configure the new Hubitat MiLight Gateway device with:
+    1. the MQTT broker address of the hub ip:port (eg. `127.0.0.1:1883`)
+    2. the MQTT topic you chose earlier (eg. `milight/status`)
+5. Save your settings - if they're correct and the hub is online, refreshing the device page will show the hub as `present` as well as listing its IP address and firmware version
---
+++ hubdriver.groovy
@@ -0,0 +1,77 @@
+/*
+ * ESP8266_MiLight_Hub presence
+ *
+ * Monitors the status of the ESP8266 MiLight Hub by sidoh - requires v1.9.0+, and an MQTT broker (https://github.com/sidoh/esp8266_milight_hub)
+ * Presence will update to 'not present' if the broker reports that the hub has gone offline.
+ * 
+ */
+metadata {
+    definition (name: "MiLight-LimitlessLED Gateway", namespace: "community", author: "cometfish") {
+        capability "PresenceSensor"
+		
+		attribute "presence", "enum", ["present", "not present"]
+    }
+}
+
+preferences {
+    section("URIs") {
+        input "mqttBroker", "string", title: "MQTT Broker Address", required: true
+		input "mqttTopic", "string", title: "MQTT Topic", required: true
+        input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true
+    }
+}
+
+import static hubitat.helper.InterfaceUtils.alphaV1mqttConnect
+import static hubitat.helper.InterfaceUtils.alphaV1mqttDisconnect
+import static hubitat.helper.InterfaceUtils.alphaV1mqttSubscribe
+import static hubitat.helper.InterfaceUtils.alphaV1parseMqttMessage
+
+def installed() {
+    log.warn "installed..."
+}
+
+def parse(String description) {
+    if (logEnable) log.debug description
+	
+    mqtt = alphaV1parseMqttMessage(description)
+	if (logEnable) log.debug mqtt
+	if (logEnable) log.debug mqtt.topic
+	
+	json = new groovy.json.JsonSlurper().parseText(mqtt.payload)
+	if (logEnable) log.debug json
+	
+	if (json.status == 'connected') 
+	    sendEvent(name: "presence", value: "present", isStateChange: true)
+	else if (json.status.contains('disconnected'))
+		sendEvent(name: "presence", value: "not present", isStateChange: true)
+	state.version = json.version
+	state.ipAddress = json.ip_address
+	state.last_reset_reason = json.reset_reason
+}
+
+def updated() {
+    log.info "updated..."
+    initialize()
+}
+
+def uninstalled() {
+    log.info "disconnecting from mqtt"
+    alphaV1mqttDisconnect(device)
+}
+
+def initialize() {
+    try {
+        //open connection
+        alphaV1mqttConnect(device, "tcp://" + settings.mqttBroker, "hubitat_milighthub", null, null)
+        //give it a chance to start
+        pauseExecution(1000)
+        if (logEnable) log.info "connection established"
+        alphaV1mqttSubscribe(device, settings.mqttTopic)
+    } catch(e) {
+        log.debug "initialize error: ${e.message}"
+    }
+}
+
+def mqttClientStatus(String status){
+    log.debug "mqttStatus- error: ${status}"
+}
